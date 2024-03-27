106eaf564118958febee3db683ad07d39ce14fdb
jkgrim <j.grimshaw37@gmail.com>
2024-03-27 11:35:14 -0600

    adds mountable clock component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 import Counter from "./components/Counter";
+import Clock from "./components/Clock";
 
 import "./App.css";
 
 function App() {
+  const [mounted, setMounted] = useState(false);
+
   return (
     <div className="app">
       <Counter />
+
+      {mounted && <Clock />}
+      <button onClick={() => setMounted((mounted) => !mounted)}>
+        {mounted ? "Unmount" : "Mount"}
+      </button>
+      <hr />
     </div>
   );
 }
